93cf8f225edc6b5fdcd053592a0b4a23aafff4c2
Fatymaah <fatymahmohamed@gmail.com>
2018-07-10 09:29:37 +0300

    radius

---
+++ Challenge1.js
@@ -0,0 +1,10 @@
+ let radius = readLine()
+    const PI = Math.PI 
+		//3.141592653589793 --javaScript 
+		//3.14159265359 -- googled value
+        
+    let area = PI*Math.pow(radius,2)  //PI*(radius*radius)
+    console.log(area)
+    
+    let perimeter = 2*PI*radius
+    console.log(perimeter)
